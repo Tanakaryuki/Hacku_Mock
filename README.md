69318ca16a7f088dc3336f2e6497c447a635b9a6
Tanakaryuki <tnk3257.rt@gmail.com>
2024-07-18 07:50:08 +0900

    add: WebSocket endpoint for physics data

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Hacku_Mock
+
+## コンテナの起動
+
+```
+$ docker-compose up
+```
+
+起動後、`http://localhost:8000/ws/physics` にコネクト可能  
+position と size は 0~10 のランダム整数値を返す
+
+## コンテナの停止
+
+```
+$ docker-compose down
+```
--- ws/routers.py
+++ ws/routers.py
@@ -1,15 +1,39 @@
 import asyncio
 from fastapi import APIRouter, WebSocket
+import random
 
 router = APIRouter()
 
-@router.websocket("/ws")
+@router.websocket("/ws/physics")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            await asyncio.sleep(0.1)
-            await websocket.send_text(f"ping")    
+            data = await websocket.receive_json()
+            
+            response = {
+                "senderId": data["senderId"],
+                "roomId": data["roomId"],
+                "objects": [
+                    {
+                        "objectId": "string",
+                        "layer": 0,
+                        "kinds": "OBJECT_KIND_UNKNOWN",
+                        "state": "OBJECT_STATE_UNKNOWN",
+                        "position": {
+                            "x": random.randint(0, 10),
+                            "y": random.randint(0, 10),
+                            "z": random.randint(0, 10)
+                        },
+                        "size": {
+                            "x": random.randint(0, 10),
+                            "y": random.randint(0, 10),
+                            "z": random.randint(0, 10)
+                        }
+                    }
+                ]
+            }
+            await websocket.send_json(response)
             
     except Exception as e:
         print(f"WebSocketエラー: {e}")
